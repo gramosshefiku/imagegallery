ef5a79f6ac63a87fd6809f625f221bfc6d6d03fc
Gramos <gshefiku@gmail.com>
2023-03-05 17:55:26 +0100

    async-function

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const defaultImage = document.getElementById('default-image');
 const smallImages = document.querySelectorAll('.small-images img');
 
-smallImages.forEach((smallImage) => {
-  smallImage.addEventListener('click', () => {
-    const defaultImageUrl = smallImage.src.replace('-url.jpg', '.jpg');
-    defaultImage.src = defaultImageUrl;
+async function changeDefaultImage(event) {
+  const smallImage = event.target;
+  const defaultImageUrl = smallImage.src.replace('-url.jpg', '.jpg');
+
+  const image = new Image();
+  image.src = defaultImageUrl;
+
+  await new Promise((resolve, reject) => {
+    image.onload = resolve;
+    image.onerror = reject;
   });
-});
+
+  defaultImage.src = defaultImageUrl;
+}
+
+for (let i = 0; i < smallImages.length; i++) {
+  smallImages[i].addEventListener('click', changeDefaultImage);
+}
